57854b893bede92fffff84179d76b73e9cad1b58
lucaslowhan <lucas.lowhan@hotmail.com>
2026-06-03 18:45:48 -0300

    feat: adiciona persistência com PostgreSQL, JPA e Flyway

---
+++ src/main/java/dev/lucaslowhan/urlshortener/repository/UrlRepository.java
@@ -0,0 +1,12 @@
+package dev.lucaslowhan.urlshortener.repository;
+
+import dev.lucaslowhan.urlshortener.domain.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, Long> {
+    Optional<Url> findByShortCode(String shortCode);
+}
---
+++ src/main/resources/db/migration/V1__create_urls_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE urls(
+    id BIGSERIAL PRIMARY KEY,
+    short_code VARCHAR(10) UNIQUE NOT NULL,
+    original_url TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
