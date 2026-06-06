505aa5786d136435d7e65df92ccfbc67f90d86b3
lucaslowhan <lucas.lowhan@hotmail.com>
2026-06-06 15:13:32 -0300

    feat: adiciona cache Redis com Cache-Aside Pattern

--- src/main/java/dev/lucaslowhan/urlshortener/service/UrlService.java
+++ src/main/java/dev/lucaslowhan/urlshortener/service/UrlService.java
@@ -2,17 +2,24 @@ package dev.lucaslowhan.urlshortener.service;
 
 import dev.lucaslowhan.urlshortener.domain.Url;
 import dev.lucaslowhan.urlshortener.repository.UrlRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UrlService {
+    @Value("${cache.url.ttl}")
+    private long cacheTtl;
     private UrlRepository urlRepository;
+    private RedisTemplate<String, String> redisTemplate;
 
-    public UrlService(UrlRepository urlRepository) {
+    public UrlService(UrlRepository urlRepository, RedisTemplate<String, String> redisTemplate) {
         this.urlRepository = urlRepository;
+        this.redisTemplate = redisTemplate;
     }
 
     /**
@@ -38,6 +45,23 @@ public class UrlService {
      * @return Optional contendo a entidade URL, ou vazio se o código não existir
      */
     public Optional<Url> getOriginalUrl(String shortCode){
-        return urlRepository.findByShortCode(shortCode);
+        String cachedUrl = redisTemplate.opsForValue().get(shortCode);
+        if(cachedUrl != null){
+            return Optional.of(Url.builder()
+                    .shortCode(shortCode)
+                    .originalUrl(cachedUrl)
+                    .build());
+        }
+        Optional<Url> optional = urlRepository.findByShortCode(shortCode);
+
+        optional.ifPresent(url ->
+                redisTemplate.opsForValue().set(
+                        shortCode,
+                        url.getOriginalUrl(),
+                        cacheTtl,
+                        TimeUnit.SECONDS
+                )
+        );
+        return optional;
     }
 }
